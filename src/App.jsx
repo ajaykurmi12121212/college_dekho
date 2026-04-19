2327ffa789fd894c658f96de2a64e628eecd0d1d
ajaykurmi12121212 <141719545+ajaykurmi12121212@users.noreply.github.com>
2026-04-20 03:45:05 +0530

    fix: college data mapping & routing issue

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import AdmissionChalo from "./pages/AdmissionChalo";
-import IITMadrasPage from "./pages/IITMadrasPage";
+
 import CollegeDetailPage from "./pages/CollegeDetailPage";
 import CoursesPage from "./pages/CoursesPage";
 import CourseDetailPage from "./pages/CourseDetailPage";
@@ -12,7 +12,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<AdmissionChalo />} />
-        <Route path="/iit-madras" element={<IITMadrasPage />} />
+        
         <Route path="/college/:id" element={<CollegeDetailPage />} />
         <Route path="/courses" element={<CoursesPage />} />
         <Route path="/courses/:stream" element={<CoursesPage />} />
